d0bb4d9c8ea20361853f601261729c6b8604b83a
LEONARDO LUCAS DE BRITO SILVA <163042516+leonardolucasbs@users.noreply.github.com>
2024-09-14 12:08:01 -0300

    Update calculadora.c

--- e11.c
+++ e11.c
@@ -1,18 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <locale.h>
-// isso é um função que limpa o buffer de entrada
+// isso Ã© um funÃ§Ã£o que limpa o buffer de entrada
+ 
+ 
+void limpa_tela(){
+    system("cls");
+}
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
 int main(){
     setlocale(LC_ALL,"Portuguese_Brazil");
-    int num1, num2, escolha, valido = 0;
-
-//pegando a resposta do usuário e verificando se ele está correto
+    int num1, num2, escolha;
+    int valido = 0;
+ 
+ 
+//pegando a resposta do usuÃ¡rio e verificando se ele estÃ¡ correto
+    while (true){
     while(!valido){
-        printf("Digite dois números inteiros: ");
+        printf("Digite dois nÃºmeros inteiros: ");
         if(scanf("%d %d", &num1, &num2) == 2){
            valido = 1;
         } else {
@@ -22,51 +31,70 @@ int main(){
     }
     limparBuffer();
     valido = 0;
-
+ 
+ 
     while(!valido){
-//opções para o usuário
-        printf("\n          Menu       \n");
-        printf("1    -Multiplicação- \n");
-        printf("2    -Divisão-       \n");
-        printf("3    -Adição-        \n");
-        printf("4    -Subtração-     \n");
-
-        printf("Digite a opção de sua escolha ");
-
+//opÃ§Ãµes para o usuÃ¡rio
+        printf("\n        Menu       \n");
+        printf("1    -MultiplicaÃ§Ã£o- \n");
+        printf("2    -DivisÃ£o-       \n");
+        printf("3    -AdiÃ§Ã£o-        \n");
+        printf("4    -SubtraÃ§Ã£o-     \n");
+ 
+ 
+        printf("Digite a opÃ§Ã£o de sua escolha ");
+ 
+ 
         if (scanf("%d", &escolha) == 1 && escolha >= 1 && escolha <= 4) {
             valido = 1;
         } else {
-            // esse fprint é para tratamento de erro mais correto de se usar
+            // esse fprint Ã© para tratamento de erro mais correto de se usar
             fprintf(stderr, "Os 'numeros foram digiados incorretamente! \n");
             limparBuffer();
-
+ 
+ 
         }
     }
         switch(escolha) {
             case 1:
-                printf("a multiplicação entre %d e %d é %d", num1, num2, num1 * num2);
+                printf("a multiplicaÃ§Ã£o entre %d e %d Ã© %d", num1, num2, num1 * num2);
             break;
-
+ 
+ 
             case 2:
                 if (num1 == 0 || num2 == 0){
-                    fprintf(stderr, "0 nào pode ser divisivel");
+                    fprintf(stderr, "0 nÃ o pode ser divisivel");
                     limparBuffer();
                 } else{
-                 printf("a divisão entre %d e %d é %d", num1, num2, (float)num1 / num2);
+                 printf("a divisÃ£o entre %d e %d Ã© %d", num1, num2,(float) num1 / num2);
                 }
                 break;
             case 3:
-                printf("a adicão entre %d e %d é %d", num1, num2, num1 + num2);
+                printf("a adicÃ£o entre %d e %d Ã© %d", num1, num2, num1 + num2);
             break;
-
+ 
+ 
             case 4:
-                printf("a adicão entre %d e %d é %d", num1, num2, num1 - num2);
+                printf("a adicÃ£o entre %d e %d Ã© %d", num1, num2, num1 - num2);
             break;
         }
-
-
-
-
-
-
+        int res;
+        printf("\nVocÃª deseja continuar? [1] - sim [0] - nÃ£o");
+        scanf("%d", &res);
+        if (res == 1){
+            limpa_tela();
+        }else if(res == 0){
+            break;
+        }
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 }
